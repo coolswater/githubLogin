ec88d99327ec65f6da62ddc3fe445da4364e4a02
贺小东 <wx_79515d5a780445b6a76e094b6ce9cb5f@git.code.tencent.com>
2019-04-03 14:02:19 +0800

    【提交】优化获取用户数据

--- src/GitHub.php
+++ src/GitHub.php
@@ -43,7 +43,7 @@ class GitHub{
         );
         $result= $this->getHttpResponsePOST(self::$getAccessTokenURL,$data);
         parse_str($result,$accessToken);
-    
+        
         return $accessToken;
     }
     
@@ -57,11 +57,12 @@ class GitHub{
      */
     public function getUserInfo($accessToken){
         $data = array(
-            'code'          => $accessToken,
-            'client_id'     => self::$clientId,
-            'client_secret' => self::$clientSecret
+            'access_token' => $accessToken,
         );
-        $userInfo = $this->getHttpResponsePOST(self::$getUserInfoURL,$data);
+        $headers = array(
+            'User-Agent:coolswater',
+        );
+        $userInfo = $this->getHttpResponseGET(self::$getUserInfoURL,$data,$headers);
         
         return $userInfo;
     }
@@ -69,18 +70,20 @@ class GitHub{
     /**
      * 远程获取数据，POST模式
      *
-     * @param   string    $url    指定URL完整路径地址
-     * @param   array     $param  请求的数据
+     * @param   string    $url      指定URL完整路径地址
+     * @param   array     $param    请求参数
+     * @param   array     $headers  请求头部
      *
      * @return  string    $data   远程输出的数据
      */
-    public function getHttpResponsePOST($url = '', $param = array()) {
+    public function getHttpResponsePOST($url, $param = array(), $headers=array()) {
         if (empty($url) || empty($param)) {
             return false;
         }
         $ch = curl_init();//初始化curl
         curl_setopt($ch, CURLOPT_URL,$url);//抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+        curl_setopt($ch, CURLOPT_HEADER, $headers);//设置header
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
         curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
         #设置为 1 是检查服务器SSL证书中是否存在一个公用名(common name)。
@@ -89,6 +92,7 @@ class GitHub{
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, '0');
         curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
         $data = curl_exec($ch);//运行curl
+        
         if (!$data){
             echo 'Curl error: ' . curl_error($ch);
         }
@@ -101,15 +105,17 @@ class GitHub{
      * 远程获取数据，GET模式
      *
      * @param string    $url        指定URL完整路径地址
-     * @param array     $header     头部
+     * @param array     $param      请求参数
+     * @param array     $headers     请求头部
      *
      * @return array    $output     远程输出的数据
      */
-    public function getHttpResponseGET($url,$header=null) {
+    public function getHttpResponseGET($url, $param = array(), $headers= array()) {
+        $url = $url.'?'.http_build_query($param);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        if(!empty($header)){
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        if(!empty($headers)){
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         #设置为 1 是检查服务器SSL证书中是否存在一个公用名(common name)。
@@ -117,6 +123,7 @@ class GitHub{
         #禁止 cURL 验证对等证书（peer's certificate）。要验证的交换证书可以在 CURLOPT_CAINFO 选项中设置
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, '0');
         $data = curl_exec($ch);
+        echo curl_getinfo($ch, CURLINFO_HEADER_OUT);
         if (!$data){
             echo 'Curl error: ' . curl_error($ch);
         }
